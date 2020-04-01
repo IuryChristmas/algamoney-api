edae36bd8e515701c62e98e5f0cb6a43e33ca7e6
iuryChristmas <iury.christmas@gmail.com>
2020-03-31 21:47:50 -0300

    correcao ceps

--- src/main/resources/db/migration/V02__CREATE_AND_REGISTER_PESSOA.sql
+++ src/main/resources/db/migration/V02__CREATE_AND_REGISTER_PESSOA.sql
@@ -11,9 +11,9 @@ estado varchar(100),
 ativo boolean not null
 );
 
-insert into pessoa (nome, logradouro, numero, bairro, cep, cidade, estado, ativo) values ('Iury', 'R Tarrafas', '97', 'Barra do Ceará', '60332520', 'Fortaleza', 'CE', true);
-insert into pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) values ('Josi', 'Rua José Marcelino Pinto', '1001 fundos', 'casa 2','Bairro Pires', '06950000', 'Juquitiba', 'SP', true);
-insert into pessoa (nome, logradouro, numero, bairro, cep, cidade, estado, ativo) values ('Andrea', 'R Cristal', '96', 'Carlito Pamplona', '60326040', 'Fortaleza', 'CE', true);
-insert into pessoa (nome, logradouro, numero, bairro, cep, cidade, estado, ativo) values ('Carlos', 'Vila Valença', '115', 'Moura Brasil', '60010030', 'Fortaleza', 'CE', true);
-insert into pessoa (nome, logradouro, numero, bairro, cep, cidade, estado, ativo) values ('Dario', 'Rua Adarias de Lima ', '15', 'Moura Brasil', '60010130', 'Fortaleza', 'CE', true);
-insert into pessoa (nome, logradouro, numero, bairro, cep, cidade, estado, ativo) values ('Marcela', 'Avenida Filomeno Gomes', '593', 'Jacarecanga', '60010280', 'Fortaleza', 'CE', true);
+insert into pessoa (nome, logradouro, numero, bairro, cep, cidade, estado, ativo) values ('Iury', 'R Tarrafas', '97', 'Barra do Ceará', '60.332-520', 'Fortaleza', 'CE', true);
+insert into pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) values ('Josi', 'Rua José Marcelino Pinto', '1001 fundos', 'casa 2','Bairro Pires', '06.950-000', 'Juquitiba', 'SP', true);
+insert into pessoa (nome, logradouro, numero, bairro, cep, cidade, estado, ativo) values ('Andrea', 'R Cristal', '96', 'Carlito Pamplona', '60.326-040', 'Fortaleza', 'CE', true);
+insert into pessoa (nome, logradouro, numero, bairro, cep, cidade, estado, ativo) values ('Carlos', 'Vila Valença', '115', 'Moura Brasil', '60.010-030', 'Fortaleza', 'CE', true);
+insert into pessoa (nome, logradouro, numero, bairro, cep, cidade, estado, ativo) values ('Dario', 'Rua Adarias de Lima ', '15', 'Moura Brasil', '60.010-130', 'Fortaleza', 'CE', true);
+insert into pessoa (nome, logradouro, numero, bairro, cep, cidade, estado, ativo) values ('Marcela', 'Avenida Filomeno Gomes', '593', 'Jacarecanga', '60.010-280', 'Fortaleza', 'CE', true);
